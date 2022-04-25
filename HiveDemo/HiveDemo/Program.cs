1fa1645b1758b9d8efb2899b4b8d1516daffb4b2
catcherwong <catcher_hwq@outlook.com>
2022-04-25 22:30:39 +0800

    feat: hive demo

---
+++ HiveDemo/HiveDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Odbc;
+
+SqlMapper.SetTypeMap(typeof(QueryResult), new CustomPropertyTypeMap(
+    typeof(QueryResult), (type, columnName) => type.GetProperties().FirstOrDefault(prop =>
+    prop.GetCustomAttributes(false).OfType<ColumnAttribute>().Any(attr => attr.Name == columnName))));
+
+var data = HiveHelper.QueryRawData();
+
+foreach (var item in data)
+{
+    Console.WriteLine(item.Id + "\t\t" + item.Bucket);
+}
+
+public static class HiveHelper
+{
+    // dsn
+    // private static string _dsn = "DSN=hivedsn;Schema=ods;UID=xxx;PWD=xxx;";
+    // dsn-less
+    private static string _dsn = "Driver=Hive;Host=xxx.xxx.xxx.xxx;Port=10000;Schema=ods;UID=xxx;PWD=xxx;";
+
+    public static List<QueryResult> QueryRawData()
+    {
+        var sql = $" select id, bucket from ods_xxx_oss where dt = '2022-01-21' limit 10;  ";
+
+        try
+        {
+            using var conn = new OdbcConnection(_dsn);
+            conn.Open();
+            var res = conn.Query<QueryResult>(sql, commandTimeout: 600000000);            
+            return res.ToList();
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+}
+
+public class QueryResult
+{
+    [Column("ods_xxx_oss.id")]
+    public string? Id { get; set; }
+
+    [Column("ods_xxx_oss.bucket")]
+    public string? Bucket { get; set; }
+}
---
+++ HiveDemo/README.md
@@ -0,0 +1,3 @@
+# HiveDemo
+
+An example used to demonstrate how to query data from [Apache Hive](https://hive.apache.org/) with C# and ODBC.
